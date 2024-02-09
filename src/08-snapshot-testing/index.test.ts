bfe79153c71737d4f9aff10adf0138d5e9fabd93
DashaErmolich <dasha.ermolich@gmail.com>
2024-02-09 13:29:23 +0100

    feat: 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,29 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
+  const ELEMENTS = [1, 2, 3];
+  const SNAPSHOT = {
+    value: ELEMENTS[0],
+    next: {
+      value: ELEMENTS[1],
+      next: {
+        value: ELEMENTS[2],
+        next: {
+          value: null,
+          next: null,
+        },
+      },
+    },
+  };
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const linkedList = generateLinkedList<number>(ELEMENTS);
+    expect(linkedList).toStrictEqual(SNAPSHOT);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const linkedList = generateLinkedList<number>(ELEMENTS);
+    expect(linkedList).toMatchSnapshot();
   });
 });
